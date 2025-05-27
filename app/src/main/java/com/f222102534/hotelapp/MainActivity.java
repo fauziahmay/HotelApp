f3e6a87017a35db0f3307c3c0a2c5242becd26e2
fauziahmay <fauziah222102534@gmail.com>
2025-05-27 14:11:24 +0700

    fauziah

---
+++ app/src/main/java/com/f222102534/hotelapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.f222102534.hotelapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        _imageView1 = findViewById(R.id.imageView1);
+        String imageUrl = "https://www.joglowisata.com/wp-content/uploads/2019/07/Embun-segoro.jpg";
+                Picasso.with(this).load(imageUrl).into(_imageView1);
+    }
+}
